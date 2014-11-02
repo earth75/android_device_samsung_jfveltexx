f18b801b994d59b587785226a3083506aa7936af
earth75 <alexis.tetar@gmail.com>
2014-11-02 21:29:01 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
+$(call inherit-product, device/samsung/jfveltexx/full_jfveltexx.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
@@ -6,8 +6,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltexx TARGET_DEVICE=jfltexx BUILD_FINGERPRINT="samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys" PRIVATE_BUILD_DESC="jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfveltexx TARGET_DEVICE=jfveltexx BUILD_FINGERPRINT="samsung/jfveltexx/jfvelte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys" PRIVATE_BUILD_DESC="jfveltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys"
 
-PRODUCT_DEVICE := jfltexx
-PRODUCT_NAME := cm_jfltexx
+PRODUCT_DEVICE := jfveltexx
+PRODUCT_NAME := cm_jfveltexx
 
